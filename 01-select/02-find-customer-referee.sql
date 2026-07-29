e01f80e5e81d161dab70804e0f47a031e34e04ba
fekrysaadallah <work.fekry@gmail.com>
2026-07-29 03:52:42 +0300

    solve find customer referee

---
+++ 01-select/02-find-customer-referee.sql
@@ -0,0 +1,11 @@
+-- LeetCode 584: Find Customer Referee
+-- Link: https://leetcode.com/problems/find-customer-referee/
+-- Topic: SELECT, WHERE
+
+SELECT
+  name
+FROM
+  Customer
+WHERE
+  referee_id <> 2
+  OR referee_id IS NULL;
