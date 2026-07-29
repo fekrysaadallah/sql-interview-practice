49ede9e66f4e3207936235c442319746cc723ed9
fekrysaadallah <work.fekry@gmail.com>
2026-07-29 03:57:36 +0300

    solve big countries

---
+++ 01-select/03-big-countries.sql
@@ -0,0 +1,11 @@
+-- LeetCode 595: Big Countries
+-- Link: https://leetcode.com/problems/big-countries/
+-- Topic: SELECT, WHERE
+
+SELECT
+  name, population, area
+FROM
+  World
+WHERE
+  area >= 3000000
+  OR population >= 25000000; 
